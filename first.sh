a218826648f5695f3b3c089263a33ff3937f7255
botkee <81105387+botkee@users.noreply.github.com>
2021-03-31 09:20:12 +0000

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,288 @@
+#!/bin/bash
+
+IP1=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+INTERFACE=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
+#IP2=
+
+IP2=ip2replace
+
+apt update && apt upgrade
+
+apt install secure-delete -y
+
+journalctl --verify 
+
+systemctl stop systemd-journald.socket; 
+systemctl stop systemd-journald-dev-log.socket; 
+systemctl stop systemd-journald-audit.socket; 
+systemctl stop systemd-journald-dev-log.socket ; 
+systemctl stop systemd-journald.socket; 
+systemctl stop systemd-journald.service; 
+systemctl disable systemd-journald.service ; 
+systemctl disable systemd-journald.socket ; 
+systemctl disable systemd-journald-dev-log.socket ; 
+systemctl disable systemd-journald-audit.socket ; 
+systemctl disable systemd-journald-dev-log.socket ; 
+systemctl disable systemd-journald.socket; 
+
+
+if [ -d "/run/log/journal"]; then 
+cd /run/log/journal
+journalctl --verify 
+srm -lvzr * 
+else
+echo "log/journal not found"
+fi
+
+
+cd 
+
+systemctl stop rsyslog
+systemctl disable rsyslog
+
+
+cd /var/log; 
+
+srm -lvzr alternatives*
+srm -lvzr auth*
+srm -lvzr btmp*
+srm -lvzr lastlog*
+srm -lvzr syslog*
+srm -lvzr bootstrap* 
+srm -lvzr daemon*
+srm -lvzr faillog*
+srm -lvzr messages* 
+srm -lvzr wtmp*
+#srm -lvzr aptitude*
+#srm -lvzr debug*
+#srm -lvzr kern*
+
+touch alternatives.log; 
+touch auth.log; 
+touch btmp; 
+touch lastlog; 
+touch syslog; 
+touch bootstrap.log; 
+touch daemon.log; 
+touch faillog; 
+touch messages; 
+touch wtmp; 
+
+cd
+
+cd /var/log
+for file in lastlog utmp wtmp alternatives auth btmp syslog bootstrap daemon faillog messages; do
+ srm -lvzr $file
+ ln -s /dev/null $file
+done
+
+cd
+
+#apt install bind9 bind9utils bind9-doc -y
+
+#Установим openvpn:
+apt install openvpn easy-rsa -y
+
+#Добавим группу nogroup и пользователя nobody, от имени этого пользователя будет работать openvpn.
+addgroup nogroup
+adduser nobody
+usermod -aG nogroup nobody
+
+
+#В каталоге /root у нас лежит архив с клиентскими сертификатами, распакуем его в /etc/openvpn
+tar -xvf /root/client.tar -C /etc/openvpn/
+
+
+#И создаем файл конфигурации, чтобы соеденить 2 сервера между собой.
+#nano /etc/openvpn/client.conf
+#С содержимым:
+
+echo -e "dev tun1
+remote $IP2
+port 443
+proto tcp-client
+ifconfig 192.168.1.2 192.168.1.1
+tls-client
+daemon
+script-security 2
+remote-cert-tls server
+ca /etc/openvpn/client-keys/ca.crt
+cert /etc/openvpn/client-keys/client.crt
+key /etc/openvpn/client-keys/client.key
+dh /etc/openvpn/client-keys/dh.pem
+tls-auth /etc/openvpn/client-keys/tls.key 1
+cipher AES-256-CBC
+persist-key
+persist-tun
+log /dev/null
+verb 0
+up /etc/openvpn/client-keys/up.sh
+down /etc/openvpn/client-keys/down.sh
+comp-lzo
+tun-mtu 1500
+user nobody
+group nogroup" > /etc/openvpn/client.conf
+
+#Создадим up\down скрипты для настройки маршрутизации трафика.
+#nano /etc/openvpn/client-keys/up.sh
+
+echo "#!/bin/sh
+ip route add default via 192.168.1.1 dev tun1 table 10
+ip rule add from 10.8.0.0/24 lookup 10 pref 10
+echo 1 > /proc/sys/net/ipv4/ip_forward" > /etc/openvpn/client-keys/up.sh
+
+#nano /etc/openvpn/client-keys/down.sh
+
+echo "#!/bin/sh
+ip route del default via 192.168.1.1 dev tun1 table 10
+ip rule del from 10.8.0.0/24 lookup 10 pref 10" > /etc/openvpn/client-keys/down.sh
+
+#Дадим им права на выполнение:
+chmod +x /etc/openvpn/client-keys/up.sh
+chmod +x /etc/openvpn/client-keys/down.sh
+
+#Добавим в автозагрузку и запустим openvpn
+systemctl enable openvpn@client
+systemctl start openvpn@client
+
+#Необходимо проверить как у нас поднялся тонель, попингуем внутренний адрес 192.168.1.1
+#ping 192.168.1.1
+
+#PING 192.168.1.1 (192.168.1.1) 56(84) bytes of data.
+#64 bytes from 192.168.1.1: icmp_seq=1 ttl=64 time=0.741 ms
+#64 bytes from 192.168.1.1: icmp_seq=2 ttl=64 time=0.842 ms
+#64 bytes from 192.168.1.1: icmp_seq=3 ttl=64 time=1.32 ms
+
+#Если ping проходит то все отлично, продолжаем. Если нет — необходимо найти причину почему не установилась связь между двумя серверами.
+
+#Генерируем сертификаты для сервера и клиентов, для этого проверим где находятся утилита для генерации сертификатов:
+
+#Узнаем путь к easy-rsa
+easyrsalocation=$(whereis easy-rsa | cut -d: -f2 | cut -c 2-)
+
+#Перейдем в каталог и приступим к генерации сертификатов для openvpn:
+cd $easyrsalocation
+
+#Генерируем CA сертификат.
+./easyrsa init-pki
+./easyrsa build-ca nopass
+
+#Генерируем сертификат сервера:
+./easyrsa build-server-full server nopass
+
+#Генерируем сертификаты клиентов меняя common name (client01):
+./easyrsa build-client-full client01 nopass
+
+#Генерируем ключ Диффи-Хеллмана:
+./easyrsa gen-dh
+
+#Генерируем ключ для tls авторизации:
+openvpn --genkey --secret pki/tls.key
+
+#Сертификаты для openvpn готовы. Теперь нам необходимо создать папку /etc/openvpn/keys/, в нее мы поместим серверные сертификаты:
+mkdir /etc/openvpn/keys
+cp -R pki/ca.crt /etc/openvpn/keys/
+cp -R pki/dh.pem /etc/openvpn/keys/
+cp -R pki/tls.key /etc/openvpn/keys/
+cp -R pki/private/server.key /etc/openvpn/keys/
+cp -R pki/issued/server.crt /etc/openvpn/keys/
+
+#Создадим файл для хранения присвоенных внутренних адресов клиентам:
+touch /etc/openvpn/keys/ipp.txt
+
+#Созадем конфигурационный файл для openvpn:
+#nano /etc/openvpn/server.conf
+
+#С содержимым:
+
+echo -e "port 443
+proto tcp
+dev tun
+sndbuf 0
+rcvbuf 0
+ca keys/ca.crt
+cert keys/server.crt
+key keys/server.key
+dh keys/dh.pem
+auth SHA512
+tls-auth keys/tls.key 0
+topology subnet
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist keys/ipp.txt
+push \"redirect-gateway def1 bypass-dhcp\"
+push \"dhcp-option DNS 8.8.8.8\"
+push \"dhcp-option DNS 8.8.4.4\"
+keepalive 10 120
+cipher AES-256-CBC
+user nobody
+group nogroup
+persist-key
+persist-tun
+verb 0" > /etc/openvpn/server.conf
+
+#Добавляем сервис openvpn в автозагрузку:
+systemctl enable openvpn@server
+
+#И запускаем его:
+systemctl start openvpn@server
+
+
+#Создаем skeleton в который допишем сертификаты
+echo -e "client
+dev tun
+proto tcp
+remote $IP1 443
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+auth SHA512
+cipher AES-256-CBC
+ignore-unknown-option block-outside-dns
+block-outside-dns
+verb 3
+tls-auth tls.key 1" > /root/client
+
+
+
+# Сгенерируем 20 сертификатов
+# start=$1
+# end=$2
+
+start=1
+end=20
+for ((i=start; i<=end; i++))
+do
+   
+cd /usr/share/easy-rsa
+./easyrsa build-client-full client0$i nopass
+
+BASE_CONFIG=/root/client
+KEY_DIR=/usr/share/easy-rsa/pki/private
+KEY_CA_DIR=/usr/share/easy-rsa/pki
+CRT_DIR=/usr/share/easy-rsa/pki/issued
+
+OUTPUT_DIR=/root/configs
+
+mkdir /root/configs
+
+cat ${BASE_CONFIG} \
+      <(echo -e '<ca>') \
+      ${KEY_CA_DIR}/ca.crt \
+      <(echo -e '</ca>\n<cert>') \
+      ${CRT_DIR}/client0${i}.crt \
+      <(echo -e '</cert>\n<key>') \
+      ${KEY_DIR}/client0${i}.key \
+      <(echo -e '</key>\n<tls-auth>') \
+      ${KEY_CA_DIR}/tls.key \
+      <(echo -e '</tls-auth>') \
+	  <(echo -e '<dh>') \
+	  ${KEY_CA_DIR}/dh.pem \
+	  <(echo -e '</dh>') \
+      > ${OUTPUT_DIR}/client0${i}.ovpn
+
+done
+
+cd /root/configs
+ls
