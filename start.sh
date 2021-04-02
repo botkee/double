46c5a0dd9cb9e93faffc9b04d93bc1ff723bc122
botkee <81105387+botkee@users.noreply.github.com>
2021-04-02 18:43:34 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,16 +5,25 @@
 # wget https://raw.githubusercontent.com/botkee/double/main/start.sh && chmod +x start.sh && bash start.sh
 #
 
+NORMAL=`echo "\033[m"`
+BRED=`printf "\e[1;31m"`
+BGREEN=`printf "\e[1;32m"`
+BYELLOW=`printf "\e[1;33m"`
+
+
+
 apt update && apt upgrade -y
 
 apt install tor -y
 
-echo "after successful login please run wget https://raw.githubusercontent.com/botkee/double/main/second.sh && chmod +x second.sh && bash second.sh"
+echo -e "${BRED}After successful login please copy and run next command${NORMAL} :"
+echo ""
+echo -e "${BGREEN}wget https://raw.githubusercontent.com/botkee/double/main/second.sh && chmod +x second.sh && bash second.sh && exit${NORMAL}"
 
 echo -e "Enter IP of second server: eg 222.222.222.222  : "; read IP2;
 
 torsocks ssh root@$IP2
 
-torsocks scp -T root@IP2:"/root/client.tar /root/run1.sh" /root/
+torsocks scp -T root@$IP2:"/root/client.tar /root/run1.sh" /root/
 
 bash /root/run1.sh
