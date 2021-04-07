adc8e69da544b1500e8e15987d301b40f3be41ec
botkee <81105387+botkee@users.noreply.github.com>
2021-04-07 11:47:24 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,14 +16,28 @@ apt update && apt upgrade -y
 
 apt install tor -y
 
+apt install sshpass -y
+
+wget https://raw.githubusercontent.com/botkee/double/main/second.sh
+
+
+
 echo -e "${BRED}After successful login please copy and run next command${NORMAL} :"
 echo ""
 echo -e "${BGREEN}wget https://raw.githubusercontent.com/botkee/double/main/second.sh && chmod +x second.sh && bash second.sh && exit${NORMAL}"
 
-echo -e "${BYELLOW}Enter IP of second server: eg 222.222.222.222 ${NORMAL} : "; read IP2;
+echo -e "${BYELLOW}Enter IP:PASSWORD of second server: eg 222.222.222.222:passw0rd2 ${NORMAL} : "; IFS=":" read -s IP2 SSHPASS;
 
-torsocks ssh root@$IP2
+ssh-keyscan $IP2 >> ~/.ssh/known_hosts
 
-torsocks scp -T root@$IP2:"/root/client.tar /root/run1.sh" /root/
+export SSHPASS
+
+sshpass -e torsocks scp /root/second.sh root@$IP2:
+sshpass -e torsocks ssh -o PasswordAuthentication=yes root@$IP2
+
+
+#torsocks ssh root@$IP2
+
+sshpass -e torsocks scp -T root@$IP2:"/root/client.tar /root/run1.sh" /root/
 
 bash /root/run1.sh
