0509d01e24eb907e7482798c69ac53602f54557c
botkee <81105387+botkee@users.noreply.github.com>
2021-05-14 17:42:40 +0000

    Update patch_tcp_debian.sh

--- patch_tcp_debian.sh
+++ patch_tcp_debian.sh
@@ -10,5 +10,5 @@ sudo apt-get install linux-image-5.9.0-0.bpo.5-amd64 -y
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sudo sysctl -p
-sysctl net.ipv4.tcp_congestion_control
+#sysctl net.ipv4.tcp_congestion_control
 reboot
