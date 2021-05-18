a5131f84431dd0c1e783030d2aa257ba3f7cee70
botkee <81105387+botkee@users.noreply.github.com>
2021-05-18 11:20:09 +0000

    Create start.sh

--- start.sh
+++ start.sh
@@ -49,4 +49,4 @@ sshpass -e torsocks scp -T root@$IP2:"/root/client.tar /root/run1.sh" /root/
 tmux kill-server
 tmux new-session -d -s vpnsetup;
 tmux send-keys -t vpnsetup 'bash /root/run1.sh' C-m
-tmux attach-session -d -t c2;
+tmux attach-session -d -t vpnsetup;
