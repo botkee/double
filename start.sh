2a27639a93b46fd60eea900a83e5317212a032c0
botkee <81105387+botkee@users.noreply.github.com>
2021-05-18 11:27:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,11 +23,7 @@ apt install sshpass -y
 wget https://raw.githubusercontent.com/botkee/double/main/second.sh
 wget -O run1.sh https://raw.githubusercontent.com/botkee/double/master/first.sh
 
-
-echo -e "${BRED}After successful login please copy and run next command${NORMAL} :"
 echo ""
-echo -e "${BGREEN}chmod +x second.sh && bash second.sh${NORMAL}"
-
 echo -e "${BYELLOW}Enter IP:PASSWORD of second server: eg 222.222.222.222:passw0rd2 ${NORMAL} : "; IFS=":" read -s IP2 SSHPASS;
 
 ssh-keyscan $IP2 >> ~/.ssh/known_hosts
@@ -40,13 +36,9 @@ rm /root/run1.sh
 #rm /root/second.sh
 #sshpass -e torsocks ssh -o PasswordAuthentication=yes root@$IP2 "chmod +x second.sh; bash -s second.sh"
 
+
 sshpass -e torsocks ssh -o PasswordAuthentication=yes root@$IP2 'bash -s' < ~/./second.sh
 
 #torsocks ssh root@$IP2
 
 sshpass -e torsocks scp -T root@$IP2:"/root/client.tar /root/run1.sh" /root/
-
-tmux kill-server
-tmux new-session -d -s vpnsetup;
-tmux send-keys -t vpnsetup 'bash /root/run1.sh' C-m
-tmux attach-session -d -t vpnsetup;
